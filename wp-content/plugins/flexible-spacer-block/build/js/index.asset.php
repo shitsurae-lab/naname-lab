4d9cfb4c770323ed22f1b8422913a398cacf4202
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-03-19 18:30:38 +0900

    update flexible spacer block

--- wp-content/plugins/flexible-spacer-block/build/js/index.asset.php
+++ wp-content/plugins/flexible-spacer-block/build/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '12441c0ca763c277d8f8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '49cda57a4da71821ecfe');
