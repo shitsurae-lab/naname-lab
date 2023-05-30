81a343a9f6c9696d9c6eeaa8971a76f91dd6330e
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-05-30 13:06:24 +0900

    update: sidebar カスタムタクソノミー、親タームをサイドバーに表示 containningの表記を削除(str_replace)

--- wp-content/plugins/my-original-function/function/custom_post_sidebar_2.php
+++ wp-content/plugins/my-original-function/function/custom_post_sidebar_2.php
@@ -76,6 +76,47 @@ function custom_post_sidebar()
     }
   // END is_object_in_term($post->ID, 'achievement_cat', ['ecommerce', 'woocommerce', 'wordpress', 'landing-page']) && is_singular('achievement'))
 
+  elseif (is_object_in_term($post->ID, 'achievement_cat', ['containing-ecommerce', 'containing-woocommerce', 'containing-wordpress', 'containing-landing-page', 'containing-banner']) && is_singular('achievement')) :
+    $terms = get_the_terms($post->ID,  'achievement_cat');
+
+    foreach ($terms as $term) {
+      $term_name = $term->name;
+      $term_name = str_replace('Containing ', '', $term_name);
+      $term_slug = $term->slug;
+      $term_slug = str_replace('containing-', '', $term_slug);
+      $term_link = get_term_link($term_slug, 'achievement_cat');
+    ?>
+      <div class="p-sidebar__content">
+        <div class="p-sidebar__heading">
+          <p class="p-sidebar__date"><?php the_date('Y.m.d'); ?></p>
+          <!-- 見出しに$term-nameがない場合はfalse(非表示)に: 条件分岐 -->
+          <?php if ($term) : ?>
+            <p id="recent-posts" class="p-newsList__heading">
+              <?php echo "<a href=\"" . $term_link . "\">" .  $term_name . "</a>"; ?>
+            </p>
+
+        </div>
+      <?php endif; ?>
+      <div class="p-sidebar__share">
+        <?php $icon = plugins_url('my-original-plugin/assets/dist/media/my-twitter-spindle-min.svg'); ?>
+        <?php $icon_fb = plugins_url('my-original-plugin/assets/dist/media/my-facebook-min.svg'); ?>
+        <!-- <p class="u-en u-uppercase">share</p> -->
+        <ul>
+          <li>
+            <a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_title_attribute(); ?>" ; target="_blank"><img src="<?php echo esc_url($icon); ?>" alt=""></a>
+          </li>
+          <li>
+            <a href="http://www.facebook.com/share.php?u=<?php the_permalink(); ?>" target="_blank"><img src="<?php echo esc_url($icon_fb); ?>" alt=""></a>
+          </li>
+        </ul>
+      </div>
+      </div>
+      <!-- END //.p-sidebar__content -->
+
+    <?php
+    }
+  // END is_object_in_term($post->ID, 'achievement_cat', ['ecommerce', 'woocommerce', 'wordpress', 'landing-page']) && is_singular('achievement'))
+
   elseif (is_object_in_term($post->ID, 'achievement_tag', ['design', 'building-website', 'working-on']) && is_singular('achievement')) :
     $terms = get_the_terms($post->ID,  'achievement_tag');
 
@@ -114,6 +155,7 @@ function custom_post_sidebar()
     <?php
     }
   // END is_object_in_term($post->ID, 'achievement_tag', ['design', 'building-website', 'working-on']) && is_singular('achievement'))
+
   elseif (is_singular('skill')) :
     global $post;
     $terms = get_the_terms($post->ID,  'skill_cat');
