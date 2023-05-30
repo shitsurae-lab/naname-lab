ffa362e978345c8882be35bd7626313ad3a59af8
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-05-30 10:17:34 +0900

    Upload: is_object_in_term && is_singular 条件分岐: カスタム投稿タイプ個別投稿ページ → タクソノミー&タームとカスタム投稿タイプ個別投稿ページに変更

--- wp-content/plugins/my-original-function/function/custom_post_sidebar_2.php
+++ wp-content/plugins/my-original-function/function/custom_post_sidebar_2.php
@@ -2,6 +2,7 @@
 //サイドバーカスタム(投稿詳細ページ)
 function custom_post_sidebar()
 {
+  global $post;
   if (is_singular('post')) :
     $cat = get_the_category();
     $cat = $cat[0];
@@ -36,8 +37,7 @@ function custom_post_sidebar()
     </div>
     <!-- END //."p-leftside -->
     <?php
-  elseif (is_singular('achievement')) :
-    global $post;
+  elseif (is_object_in_term($post->ID, 'achievement_cat', ['ecommerce', 'woocommerce', 'wordpress', 'landing-page', 'banner']) && is_singular('achievement')) :
     $terms = get_the_terms($post->ID,  'achievement_cat');
 
     foreach ($terms as $term) {
@@ -74,6 +74,46 @@ function custom_post_sidebar()
 
     <?php
     }
+  // END is_object_in_term($post->ID, 'achievement_cat', ['ecommerce', 'woocommerce', 'wordpress', 'landing-page']) && is_singular('achievement'))
+
+  elseif (is_object_in_term($post->ID, 'achievement_tag', ['design', 'building-website', 'working-on']) && is_singular('achievement')) :
+    $terms = get_the_terms($post->ID,  'achievement_tag');
+
+    foreach ($terms as $term) {
+      $term_name = $term->name;
+      $term_slug = $term->slug;
+      $term_link = get_term_link($term_slug, 'achievement_tag');
+    ?>
+      <div class="p-sidebar__content">
+        <div class="p-sidebar__heading">
+          <p class="p-sidebar__date"><?php the_date('Y.m.d'); ?></p>
+          <!-- 見出しに$term-nameがない場合はfalse(非表示)に: 条件分岐 -->
+          <?php if ($term) : ?>
+            <p id="recent-posts" class="p-newsList__heading">
+              <?php echo "<a href=\"" . $term_link . "\">" .  $term_name . "</a>"; ?>
+            </p>
+
+        </div>
+      <?php endif; ?>
+      <div class="p-sidebar__share">
+        <?php $icon = plugins_url('my-original-plugin/assets/dist/media/my-twitter-spindle-min.svg'); ?>
+        <?php $icon_fb = plugins_url('my-original-plugin/assets/dist/media/my-facebook-min.svg'); ?>
+        <!-- <p class="u-en u-uppercase">share</p> -->
+        <ul>
+          <li>
+            <a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_title_attribute(); ?>" ; target="_blank"><img src="<?php echo esc_url($icon); ?>" alt=""></a>
+          </li>
+          <li>
+            <a href="http://www.facebook.com/share.php?u=<?php the_permalink(); ?>" target="_blank"><img src="<?php echo esc_url($icon_fb); ?>" alt=""></a>
+          </li>
+        </ul>
+      </div>
+      </div>
+      <!-- END //.p-sidebar__content -->
+
+    <?php
+    }
+  // END is_object_in_term($post->ID, 'achievement_tag', ['design', 'building-website', 'working-on']) && is_singular('achievement'))
   elseif (is_singular('skill')) :
     global $post;
     $terms = get_the_terms($post->ID,  'skill_cat');
