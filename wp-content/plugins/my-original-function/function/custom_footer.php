5524a0723c11534e0d2d3c156a8a4192747644b4
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-06-20 08:16:48 +0900

    Modify: custom_footer.php l-footerの下層をp-footerとして書き換え

--- wp-content/plugins/my-original-function/function/custom_footer.php
+++ wp-content/plugins/my-original-function/function/custom_footer.php
@@ -2,19 +2,19 @@
 function start_footer_foot()
 {
   $upload_dir = wp_upload_dir();
-  echo '<div class="l-footer__nav__wrapper"><div class="l-footer__logo"><a href="' . home_url() . '"><img src="' . esc_url($upload_dir['baseurl'] . '/2022/12/logo_name_type_white.svg') . '" alt=""></a></div>';
+  echo '<ul class="p-footer"><li><ul><li class="p-footer__logo"><a href="' . home_url() . '"><figure><img src="' . esc_url($upload_dir['baseurl'] . '/2022/12/logo_name_type_white.svg') . '" alt="ななめラボ"></figure></a></li><!-- END p-footer__logo--><li class="p-footer__text"><h2>ななめラボ</h2><ul class="p-footer__text--list"><li class="p-footer__text--ttl">follow</li><li><i class="fab fa-twitter fa-fw icon--primary"></i></li><li><i class="fab fa-facebook-f fa-fw icon--primary"></i></li></ul></li><!-- END //.p-footer__text__inner --></ul></li><!-- END //.p-footer__text --><li>';
 }
 add_action('arkhe_start_footer_foot_content', 'start_footer_foot');
 
 function before_copyright()
 {
-  echo '</div><div class="l-footer__sns"><nav class="l-footer__sns__container"><ul class="c-sns__list"><li class="c-sns__item"><i class="fab fa-twitter fa-fw"></i></li><li class="c-sns__item"><i class="fab fa-facebook-f fa-fw"></i></li></ul></nav><div class="l-footer__copyright">';
+  echo '</ul><div class="l-footer__copyright">';
 }
 // add_action('arkhe_start_footer_foot_content', 'insert_footer__nav');
 add_action('arkhe_before_copyright', 'before_copyright');
 
 function after_copyright()
 {
-  echo '</div><!-- END //.l-footer__copyright --></div><!-- END //.l-footer__sns__wrapper -->';
+  echo '</div><!-- END //.l-footer__copyright -->';
 }
 add_action('arkhe_after_copyright', 'after_copyright');
