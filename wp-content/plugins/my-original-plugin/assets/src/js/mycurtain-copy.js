c32bb3252073e1afb4401d852b6f7ec413e541b2
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-06-30 08:04:47 +0900

    modify curtain.scss & curtain.js オープニングアニメーションの改定

--- wp-content/plugins/my-original-plugin/assets/src/js/mycurtain-copy.js
+++ wp-content/plugins/my-original-plugin/assets/src/js/mycurtain-copy.js
@@ -4,7 +4,7 @@ const mycurtain = () => {
 
   //②カーテン要素の作成
   const curtainElement = document.createElement('div');
-  curtainElement.setAttribute('class', 'p-curtain');
+  curtainElement.setAttribute('class', 'curtain');
 
   //③loaderを作成
   const loader = document.createElement('div');
@@ -33,19 +33,49 @@ const mycurtain = () => {
     console.log(opening_inner_elem);
   }
 
-  function js_curtain() {
+  function js_secondary() {
     curtainElement.style.display = 'none';
     console.log('②番目だよ');
     console.log('カーテン要素をnoneに');
   }
 
-  function js_pageOn() {
+  function js_primary() {
     document.body.classList.add('pageOn');
     console.log('①番目だよ');
     console.log('bodyにpageOn');
   }
+  // setTimeout(() => {
+  //   setTimeout(() => {
+  //     //カーテン要素をdisplay:noneに
+  //     curtainElement.style.display = 'none';
+  //     console.log('②番目だよ');
+  //     console.log('カーテン要素をnoneに');
+  //   }, 1500);
+  //   //bodyにclass'pageOn'を付加
+  //   document.body.classList.add('pageOn');
+  //   console.log('①番目だよ');
+  //   console.log('bodyにpageOn');
+  // });
+  //END: カーテンをdisplay:none; & Bodyに"pageOn"を付加(関数に入れない)
 
   /*コールバック関数 */
+
+  //カーテンフェードイン & フェードアウト
+  //-- 下記functionは記述に誤りあり。一旦コメントアウト
+  function curtainFadeInOut() {
+    setTimeout(() => {
+      setTimeout(() => {
+        bodyElement.style.display = 'none';
+        console.log('④bodyがstyle:noneとなる');
+      }, 3000); //デフォルト数値は1000
+      bodyElement.classList.add('fadeout');
+      console.log('③bodyにfadeoutが付加される');
+    });
+  }
+
+  // function spinnerFadeOut() {
+  //   //ここにカーテンフェードインとスピナーフェードアウトを書く
+  // }
 
   // ページ更新時、最初だけローディングアニメーション
   //①beforeunloadとunloadは「更新検知」
@@ -67,10 +97,12 @@ const mycurtain = () => {
     let $i = check_access();
     if ($i == 0) {
       //1回目アクセスの処理
+      // const loader = document.querySelector('.p-loader');
+      //START: ローディング
       setTimeout(() => {
         setTimeout(() => {
           setTimeout(() => {
-            // js_curtain();
+            js_secondary();
             console.log('1回目のアクセスの際の「3番めの処理です」');
           }, 1000);
           opening.classList.add('loaded');
@@ -78,7 +110,7 @@ const mycurtain = () => {
           console.log('1回目のアクセスの際の「2番めの処理です」');
         }, 2000);
         loader.classList.add('loaded');
-        js_pageOn();
+        js_primary();
         console.log('1回目のアクセスの際の「1番めの処理です」');
       }, 0);
     } else {
@@ -87,11 +119,10 @@ const mycurtain = () => {
       setTimeout(() => {
         setTimeout(() => {
           // curtainFadeInOut();
-          // js_curtain();
-          opening.classList.add('loaded');
+          js_secondary();
           console.log('2回目以降のアクセスの際の「2番めの処理です」');
         }, 2000);
-        js_pageOn();
+        js_primary();
         console.log('2回目以降のアクセスの際の「1番目の処理です」');
       }, 0);
       // loader.remove();
