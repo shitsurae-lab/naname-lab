4cc255fb317fd923aa09ea44b946061d953bf22d
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-07-01 22:07:51 +0900

    compile js to main.js & main.js.map

--- wp-content/plugins/my-original-plugin/assets/dist/js/main.js
+++ wp-content/plugins/my-original-plugin/assets/dist/js/main.js
@@ -275,7 +275,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-const setObserver = () => {
+const intersection = () => {
   //①コールバック関数
   const callback = (entries) => {
     entries.forEach((entry) => {
@@ -290,7 +290,7 @@ const setObserver = () => {
   };
   //②オプション
   const options = {
-    root: null,
+    root: null, //ルート
     rootMargin: '-10% 0px',
     threshold: 0.1,
   };
@@ -300,7 +300,6 @@ const setObserver = () => {
 
   //④監視対象の要素をすべて取得
   const [...monitorElements] = document.querySelectorAll('.js-target');
-  console.log(monitorElements);
 
   //⑤すべての監視対象要素をobserve()メソッドに指定
   monitorElements.forEach((elem) => {
@@ -309,129 +308,7 @@ const setObserver = () => {
   });
   console.log('Intersection Observer APIを読み込んでます');
 };
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (setObserver);
-
-
-/***/ }),
-
-/***/ "./assets/src/js/mycurtain.js":
-/*!************************************!*\
-  !*** ./assets/src/js/mycurtain.js ***!
-  \************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-const mycurtain = () => {
-  //① bodyタグを取得
-  const bodyElement = document.querySelector('body');
-
-  //②spinnnerエリア
-  const opening = document.createElement('div');
-  opening.setAttribute('class', 'p-spinner__wrapper');
-  //②-1 spinnerを作成
-  const spinner = document.createElement('div');
-  spinner.setAttribute('class', 'p-spinner');
-  //③loadingアニメーション
-  const loader = document.createElement('div');
-  loader.setAttribute('class', 'p-loader');
-  const loaderContent = document.createElement('div');
-  loaderContent.setAttribute('class', 'p-loader__container');
-  loader.prepend(loaderContent);
-  const loaderText = document.createElement('p');
-  loaderText.setAttribute('class', 'p-loader__text u-uppercase');
-  loaderContent.prepend(loaderText);
-  loaderText.innerText = 'Let me introduce\nmyself';
-
-  //④カーテン要素の作成
-  const curtainElement = document.createElement('div');
-  curtainElement.setAttribute('class', 'p-curtain');
-
-  //①body要素内の先頭にp-loader
-  bodyElement.prepend(opening);
-  //②p-opening要素内の先頭にp-spinner
-  opening.prepend(spinner);
-  //③ p-openingの後ろにp-loader
-  opening.after(loader);
-  //④ p-loaderの後ろにカーテン要素
-  loader.after(curtainElement);
-
-  const js_opening = () => {
-    opening.classList.add('loaded');
-    console.log('①js_openingが読み込まれたよ!');
-  };
-
-  const js_loader = () => {
-    loader.classList.add('loaded');
-    console.log('②js_loaderが読み込まれたよ!');
-  };
-
-  const js_curtain = () => {
-    curtainElement.classList.add('loaded');
-    console.log('③js_curtainが読み込まれたのよ!');
-  };
-
-  const js_pageOn = () => {
-    document.body.classList.add('pageOn');
-    console.log('④ js_pageOnが読み込まれたぜ');
-  };
-
-  /*コールバック関数 */
-
-  // ページ更新時、最初だけローディングアニメーション
-  //①beforeunloadとunloadは「更新検知」
-  //window.addEventListener('load', () => { //②これは画像まで読み込まれてからの意味
-  document.addEventListener('DOMContentLoaded', () => {
-    let flg = null;
-    let check_access = function () {
-      // ★sessionStorageの値を判定
-      if (sessionStorage.getItem('access_flg')) {
-        // 2回目以降
-        flg = 1;
-      } else {
-        // 1回目
-        sessionStorage.setItem('access_flg', true);
-        flg = 0;
-      }
-      return flg;
-    };
-    let $i = check_access();
-    if ($i == 0) {
-      //1回目アクセスの処理
-      setTimeout(() => {
-        setTimeout(() => {
-          setTimeout(() => {
-            setTimeout(() => {
-              js_pageOn();
-            }, 1000); //curtaniAnime 800ms(duration)の少しあとにjs_pageOnが動くようにする
-            js_curtain();
-          }, 2000); //loaderAnime 2000ms(duration)にあわせてずらした
-          js_loader();
-        }, 1200); //spinnerAnime 1200ms(duration)にあわせてずらした
-        js_opening();
-      }, 0);
-    } else {
-      // 2回目アクセスの処理
-      setTimeout(() => {
-        setTimeout(() => {
-          setTimeout(() => {
-            setTimeout(() => {
-              js_pageOn();
-            }, 1000); //curtaniAnime 800ms(duration)の少しあとにjs_pageOnが動くようにする
-            js_curtain();
-          }, 2000); //loaderAnime 2000ms(duration)にあわせてずらした
-          js_loader();
-        }, 0);
-        opening.remove();
-      }, 0);
-    }
-  });
-};
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (mycurtain);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (intersection);
 
 
 /***/ }),
@@ -608,6 +485,129 @@ const postSwiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"]('.infinite
 });
 
 
+
+
+/***/ }),
+
+/***/ "./assets/src/js/openingAnime.js":
+/*!***************************************!*\
+  !*** ./assets/src/js/openingAnime.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//document.addEventListener('DOMContentLoaded'..がフックになるJS-----
+const openingAnime = () => {
+  //① bodyタグを取得
+  const bodyElement = document.querySelector('body');
+
+  //②spinnnerエリア
+  const opening = document.createElement('div');
+  opening.setAttribute('class', 'p-spinner__wrapper');
+  //②-1 spinnerを作成
+  const spinner = document.createElement('div');
+  spinner.setAttribute('class', 'p-spinner');
+  //③loadingアニメーション
+  const loader = document.createElement('div');
+  loader.setAttribute('class', 'p-loader');
+  const loaderContent = document.createElement('div');
+  loaderContent.setAttribute('class', 'p-loader__container');
+  loader.prepend(loaderContent);
+  const loaderText = document.createElement('p');
+  loaderText.setAttribute('class', 'p-loader__text u-uppercase');
+  loaderContent.prepend(loaderText);
+  loaderText.innerText = 'Let me introduce\nmyself';
+
+  //④カーテン要素の作成
+  const curtainElement = document.createElement('div');
+  curtainElement.setAttribute('class', 'p-curtain');
+
+  //①body要素内の先頭にp-loader
+  bodyElement.prepend(opening);
+  //②p-opening要素内の先頭にp-spinner
+  opening.prepend(spinner);
+  //③ p-openingの後ろにp-loader
+  opening.after(loader);
+  //④ p-loaderの後ろにカーテン要素
+  loader.after(curtainElement);
+
+  const js_opening = () => {
+    opening.classList.add('loaded');
+    console.log('①js_openingが読み込まれたよ!');
+  };
+
+  const js_loader = () => {
+    loader.classList.add('loaded');
+    console.log('②js_loaderが読み込まれたよ!');
+  };
+
+  const js_curtain = () => {
+    curtainElement.classList.add('loaded');
+    console.log('③js_curtainが読み込まれたのよ!');
+  };
+
+  const js_pageOn = () => {
+    document.body.classList.add('pageOn');
+    console.log('④ js_pageOnが読み込まれたぜ');
+  };
+
+  /*コールバック関数 */
+
+  // ページ更新時、最初だけローディングアニメーション
+  //①beforeunloadとunloadは「更新検知」
+  //window.addEventListener('load', () => { //②これは画像まで読み込まれてからの意味
+  document.addEventListener('DOMContentLoaded', () => {
+    let flg = null;
+    let check_access = function () {
+      // ★sessionStorageの値を判定
+      if (sessionStorage.getItem('access_flg')) {
+        // 2回目以降
+        flg = 1;
+      } else {
+        // 1回目
+        sessionStorage.setItem('access_flg', true);
+        flg = 0;
+      }
+      return flg;
+    };
+    let $i = check_access();
+    if ($i == 0) {
+      //1回目アクセスの処理
+      setTimeout(() => {
+        setTimeout(() => {
+          setTimeout(() => {
+            setTimeout(() => {
+              js_pageOn();
+            }, 1000); //curtaniAnime 800ms(duration)の少しあとにjs_pageOnが動くようにする
+            js_curtain();
+          }, 2000); //loaderAnime 2000ms(duration)にあわせてずらした
+          js_loader();
+        }, 1200); //spinnerAnime 1200ms(duration)にあわせてずらした
+        js_opening();
+      }, 0);
+    } else {
+      // 2回目アクセスの処理
+      setTimeout(() => {
+        setTimeout(() => {
+          setTimeout(() => {
+            setTimeout(() => {
+              js_pageOn();
+            }, 1000); //curtaniAnime 800ms(duration)の少しあとにjs_pageOnが動くようにする
+            js_curtain();
+          }, 2000); //loaderAnime 2000ms(duration)にあわせてずらした
+          js_loader();
+        }, 0);
+        opening.remove();
+      }, 0);
+    }
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (openingAnime);
 
 
 /***/ }),
@@ -19092,7 +19092,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _css_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../css/style.scss */ "./assets/src/css/style.scss");
 /* harmony import */ var _css_style_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_css_style_scss__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _gnav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./gnav */ "./assets/src/js/gnav.js");
-/* harmony import */ var _mycurtain__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./mycurtain */ "./assets/src/js/mycurtain.js");
+/* harmony import */ var _openingAnime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./openingAnime */ "./assets/src/js/openingAnime.js");
 /* harmony import */ var _addSelector__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./addSelector */ "./assets/src/js/addSelector.js");
 /* harmony import */ var _intersection__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./intersection */ "./assets/src/js/intersection.js");
 /* harmony import */ var _createSVG__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./createSVG */ "./assets/src/js/createSVG.js");
@@ -19113,13 +19113,13 @@ __webpack_require__.r(__webpack_exports__);
 
 //オープニングアニメーション
 
-(0,_mycurtain__WEBPACK_IMPORTED_MODULE_2__["default"])();
+(0,_openingAnime__WEBPACK_IMPORTED_MODULE_2__["default"])();
 
 //-- addSelector
 
 (0,_addSelector__WEBPACK_IMPORTED_MODULE_3__["default"])();
 
-//Intersection Observer
+//Intersection Observe
 
 (0,_intersection__WEBPACK_IMPORTED_MODULE_4__["default"])();
 
