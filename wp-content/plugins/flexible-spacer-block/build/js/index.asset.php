5385ff8898a9ecebabe8f52849a3e4cb45161d4c
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-07-25 19:30:22 +0900

    modify plugins flexible-spacer-block

--- wp-content/plugins/flexible-spacer-block/build/js/index.asset.php
+++ wp-content/plugins/flexible-spacer-block/build/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '49cda57a4da71821ecfe');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '42ec76caddc36d5bc99f');
