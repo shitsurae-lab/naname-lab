d3ec0daa62f952927014300fa3ea805932b3cf28
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-08-02 09:10:48 +0900

    modify index.js fontawesome font指定

--- wp-content/plugins/my-original-plugin/assets/src/js/index.js
+++ wp-content/plugins/my-original-plugin/assets/src/js/index.js
@@ -75,5 +75,8 @@ particles();
 
 //--Font awesome
 import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
+// import '@fortawesome/fontawesome-free/js/solid';
+import {
+  faFolder,
+  faSquareCheck,
+} from '@fortawesome/fontawesome-free/js/regular';
