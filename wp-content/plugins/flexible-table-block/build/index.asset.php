5c253110c108e7bbea3361fc1ce9187b9ac0fc61
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-08-05 23:03:49 +0900

    update plugin--flexible-table-block

--- wp-content/plugins/flexible-table-block/build/index.asset.php
+++ wp-content/plugins/flexible-table-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '5074912ca5487be2adc2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'b478939879e9ce01ff15');
