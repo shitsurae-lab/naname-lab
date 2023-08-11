6d62d48575f72faedeefc0dc9fa93ed2c9b7ba3e
naname-lab <20566363+naname-lab@users.noreply.github.com>
2023-08-11 21:14:56 +0900

    ADD mierucaタグ

--- wp-content/plugins/my-original-plugin/my-original-plugin.php
+++ wp-content/plugins/my-original-plugin/my-original-plugin.php
@@ -114,3 +114,18 @@ height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></nos
 <!-- End Google Tag Manager (noscript) -->";
 }
 add_action('wp_body_open', 'add_gtm_body');
+
+function  add_mieruca_head()
+{
+  echo "<!-- Begin Mieruca Embed Code -->
+<script type=\"text/javascript\" id=\"mierucajs\">
+window.__fid = window.__fid || [];__fid.push([556739964]);
+(function() {
+function mieruca(){if(typeof window.__fjsld != \"undefined\") return; window.__fjsld = 1; var fjs = document.createElement('script'); fjs.type = 'text/javascript'; fjs.async = true; fjs.id = \"fjssync\"; var timestamp = new Date;fjs.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://hm.mieru-ca.com/service/js/mieruca-hm.js?v='+ timestamp.getTime(); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(fjs, x); };
+setTimeout(mieruca, 500); document.readyState != \"complete\" ? (window.attachEvent ? window.attachEvent(\"onload\", mieruca) : window.addEventListener\"load\", mieruca, false)) : mieruca();
+})();
+</script>
+<!-- End Mieruca Embed Code -->";
+}
+
+add_action('wp_enqueue_scripts', 'add_mieruca_head');
