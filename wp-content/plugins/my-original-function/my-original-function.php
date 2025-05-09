8505ff57e405e36fe592f3f8cc798ec04e91a61c
shitsurae-lab <mahalo.morganite33@gmail.com>
2025-05-09 22:10:49 +0900

    insert_front_sliderからinsert_main_contentにリネームしたのでinclude_onceの式をそれに伴い変更

--- wp-content/plugins/my-original-function/my-original-function.php
+++ wp-content/plugins/my-original-function/my-original-function.php
@@ -42,7 +42,7 @@ include_once(plugin_dir_path(__FILE__) . 'function/register_shortcode.php');
 include_once(plugin_dir_path(__FILE__) . 'function/add_opening_interaction.php');
 
 //フロントページのみにスライダーを実装
-include_once(plugin_dir_path(__FILE__) . 'function/insert_front_slider.php');
+include_once(plugin_dir_path(__FILE__) . 'function/insert_main_content.php');
 
 //カスタム投稿タイプ等にメインビジュアルを挿入
 include_once(plugin_dir_path(__FILE__) . 'function/insert_main_visual.php');
