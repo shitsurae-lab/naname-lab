a5f6d522fbec5b62f7043f4c26164ce8f0e81915
shitsurae-lab <mahalo.morganite33@gmail.com>
2025-05-18 19:52:44 +0900

    modify filter_nav_menu, initial, heading montserrat →　dosis

--- wp-content/plugins/my-original-function/function/filter_nav_menu.php
+++ wp-content/plugins/my-original-function/function/filter_nav_menu.php
@@ -3,7 +3,7 @@ add_filter('walker_nav_menu_start_el', 'insert_attr_title_in_menu', 10, 4);
 function insert_attr_title_in_menu($item_output, $item)
 {
   if (!empty($item->attr_title)) {
-    $item_output = preg_replace('/(<a.*?>[^<]*?)</', '$1' . "<span class=\"u-uppercase u-montserrat u-font--600 u-font--xxs\">{$item->attr_title}</span><", $item_output);
+    $item_output = preg_replace('/(<a.*?>[^<]*?)</', '$1' . "<span class=\"u-uppercase u-dosis u-font--600 u-font--xs\">{$item->attr_title}</span><", $item_output);
   }
   return $item_output;
 }
