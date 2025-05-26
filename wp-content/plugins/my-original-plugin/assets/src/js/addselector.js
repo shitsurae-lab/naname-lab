440d5f879b700f20f1abf8f58af16034029ce420
shitsurae-lab <mahalo.morganite33@gmail.com>
2025-05-26 19:14:29 +0900

    パスワード保護のページについてサイドバーを非表示にする。

--- wp-content/plugins/my-original-plugin/assets/src/js/addselector.js
+++ wp-content/plugins/my-original-plugin/assets/src/js/addselector.js
@@ -1,4 +1,4 @@
-function addSelector() {
+function addJSTarget() {
   const targets = document.querySelectorAll(
     '.p-front__content .ark-block-section__bodyInner'
   );
@@ -6,4 +6,19 @@ function addSelector() {
   targets.forEach((target) => target.classList.add('js-target'));
 }
 
+//パスワード保護の場合、post-password-requiredがarticleに付与されるので親要素のmainにclassをプラス
+const passwordClass = () => {
+  document.addEventListener('DOMContentLoaded', function () {
+    const protectedArticle = document.querySelector(
+      'article.post-password-required'
+    );
+    if (protectedArticle) {
+      protectedArticle.closest('main').classList.add('has-password-form');
+    }
+  });
+};
+const addSelector = () => {
+  addJSTarget();
+  passwordClass();
+};
 export default addSelector;
