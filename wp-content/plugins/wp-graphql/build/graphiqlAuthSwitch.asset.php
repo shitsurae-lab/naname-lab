89cd718baea7681627b5eea152e7f1bcc0a8850f
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-01-23 16:16:40 +0900

    Add Plugins(WPGraphQL, WPGraphQL for ACF)

---
+++ wp-content/plugins/wp-graphql/build/graphiqlAuthSwitch.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'daa1ad09d5c9664c41e8');
---
+++ wp-content/plugins/wp-graphql/build/graphiqlQueryComposer.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '30f41cd531f3556b5e65');
